e7441dd135fd6cf5888de421d3c197c6092b23b9
arberlisaj <the.arberlisaj@gmail.com>
2024-10-10 21:04:39 +0200

    feat: Format java file with 'google-java-format'

--- lua/lisajarber/remap.lua
+++ lua/lisajarber/remap.lua
@@ -9,7 +9,7 @@ vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
 -- Clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
@@ -39,11 +39,6 @@ vim.keymap.set("n", "<leader><CR>", function()
   vim.lsp.buf.execute_command({ command = "_typescript.organizeImports", arguments = { vim.fn.expand("%:p") } })
 end)
 
--- Source
-vim.keymap.set("n", "<leader><leader>", function()
-  vim.cmd("so")
-end)
-
 -- Save
 vim.keymap.set("n", "<leader>w", function()
   vim.cmd("w")
@@ -53,3 +48,23 @@ end)
 vim.keymap.set("n", "<leader>q", function()
   vim.cmd("q!")
 end)
+
+-- Format Java file
+vim.api.nvim_create_user_command(
+  'Fjava',
+  function()
+    local filepath = vim.fn.expand('%:p')
+    local cmd = { "google-java-format", "-i", filepath }
+
+    vim.fn.jobstart(cmd, {
+      on_exit = function(_, code, _)
+        if code == 0 then
+          vim.cmd('edit')
+        else
+          print("Failed to format file. Error code: " .. code)
+        end
+      end,
+    })
+  end,
+  {}
+)
