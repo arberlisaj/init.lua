d46070125314aef8984977544abb601699098599
arberlisaj <the.arberlisaj@gmail.com>
2024-12-15 11:01:09 +0100

    feat: Update lazy-lock.json and add Copilot integration

--- lua/lisajarber/lazy/colors.lua
+++ lua/lisajarber/lazy/colors.lua
@@ -1,8 +1,9 @@
 function Color(color)
   color = color or "rose-pine"
   vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+  vim.api.nvim_set_hl(0, "Normal", { bg = "black" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "black" })
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "black" })
 end
 
 return {
